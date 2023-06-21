4ac97b139570c6306b88d072b9050f8fa56cf382
JainPromudShreyansh <133496894+JainPromudShreyansh@users.noreply.github.com>
2023-06-21 23:43:24 +0530

    Update elseif_tb.v

--- MUX/Code/elseif_tb.v
+++ MUX/Code/elseif_tb.v
@@ -5,16 +5,16 @@ reg s;
 wire [2:0] y;
 mux dut(a,b,s,y);
 initial begin 
-	#10;
 	a=3;
 	b=5;
 	s=0;
-	$display("s=%0d, a= %0d, b=%0d, y=%0d",s,a,b,y);
 	#10;
-	s=1;
 	$display("s=%0d, a= %0d, b=%0d, y=%0d",s,a,b,y);
-	#10;
 	s=1;
+	#10;
+	$display("s=%0d, a= %0d, b=%0d, y=%0d",s,a,b,y);
+	s=0;
+	#10;
 	$display("s=%0d, a= %0d, b=%0d, y=%0d",s,a,b,y);
 end
 
