16e2515ba1dd7f26734ccd583b5aa90437e4ab57
JainPromudShreyansh <133496894+JainPromudShreyansh@users.noreply.github.com>
2023-07-26 23:00:50 +0530

    Update four_bit_gray_counter.v

--- Counter/four_bit_gray_counter.v
+++ Counter/four_bit_gray_counter.v
@@ -4,10 +4,11 @@ input clk,rst;
 input [n-1:0]Bin;
 output reg [n-1:0]Gout;
 if (rst) Gout=0;
-else
+else begin
 Gout[3]<=Bin[3];
 Gout[2]<=Bin[3] xor Bin[2];
 Gout[1]<=Bin[2] xor Bin[1];
 Gout[1]<=Bin[1] xor Bin[0];
+end
 endmodule
 
